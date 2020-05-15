f6bcbebd1e453359084bad9dad9e251a974eccc7
Suraj Gupta <43107348+kaflin@users.noreply.github.com>
2020-05-15 21:25:05 +0545

    Add files via upload

---
+++ user-account-registration/src/main/java/com/codebysuraj/useraccountregistration/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.codebysuraj.useraccountregistration.repository;
+
+import com.codebysuraj.useraccountregistration.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository("userRepository")
+public interface UserRepository extends CrudRepository<User,Long> {
+    User findByEmail(String email);
+    User findByConfirmationToken(String confirmationToken);
+
+    User findByEmailAndPassword(String email, String password);
+}
---
+++ user-account-registration/src/main/java/com/codebysuraj/useraccountregistration/service/UserService.java
@@ -0,0 +1,37 @@
+package com.codebysuraj.useraccountregistration.service;
+
+import com.codebysuraj.useraccountregistration.model.User;
+import com.codebysuraj.useraccountregistration.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service("userService")
+public class UserService {
+
+   @Autowired
+   private UserRepository userRepository;
+
+//    @Autowired
+//    public UserService(UserRepository userRepository)
+//    {
+//        this.userRepository=userRepository;
+//    }
+    public User findByEmail(String email)
+    {
+        return userRepository.findByEmail(email);
+
+    }
+    public User findByConfirmationToken(String confirmationToken) {
+        return userRepository.findByConfirmationToken(confirmationToken);
+    }
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User findByEmailAndPassword(String email, String password) {
+        return userRepository.findByEmailAndPassword(email,password);
+    }
+}
+
+
